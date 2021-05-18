cfbf549d16c4299fc38c14e5f8d3d3ca12c343e8
test197106 <76862379+test197106@users.noreply.github.com>
2021-05-18 12:11:20 +0530

    Add files via upload

---
+++ allsqmsg.sql
@@ -0,0 +1,49 @@
+Pinging SQL infra
+Selecting from table1
+Selecting from table2
+Selecting from table3
+Creating t_events table in p1s0 database
+Creating t_files table in p1s0 database
+Creating t_fns table in p1s0 database
+Creating t_runs table in p1s0 database
+Selecting from table4
+Selecting from table5
+Selecting from table6
+Selecting from table7
+Populating t_events table in p1s0 database
+Populating t_files table in p1s0 database
+Populating t_fns table in p1s0 database
+Populating t_runs table in p1s0 database
+Selecting from table8
+Selecting t_events table from p1s0 database
+Selecting t_files table from p1s0 database
+Selecting t_fns table from p1s0 database
+Selecting t_runs table from p1s0 database
+Selecting from table9
+Selecting from tableA
+Selecting from tableB
+Selecting from tableC
+Selecting from tableD
+Creating t_events table in p1s1 database
+Creating t_files table in p1s1 database
+Populating t_events table in p1s1 database
+Populating t_files table in p1s1 database
+Selecting t_events table from p1s1 database
+Selecting t_files table from p1s1 database
+Creating t_events table in p2s2 database
+Creating t_files table in p2s2 database
+Creating t_fns table in p2s2 database
+Creating t_runs table in p2s2 database
+Populating t_events table in p2s2 database
+Populating t_files table in p2s2 database
+Populating t_fns table in p2s2 database
+Populating t_runs table in p2s2 database
+Selecting t_events table from p2s2 database
+Selecting t_files table from p2s2 database
+Selecting t_fns table from p2s2 database
+Selecting t_runs table from p2s2 database
+Selecting from temporary table temp_table1
+Creating t_runs table in p3s5 database
+Populating t_runs table in p3s5 database
+Selecting t_runs table from p3s5 database
+The purpose of this SQL
